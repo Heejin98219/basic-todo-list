ee62dd70ef92cab92f3179983107d19ec21fd164
whgmlwls0219 <whgmlwls0219@naver.com>
2024-09-19 18:06:48 +0900

    컴포넌트 생성

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./index.css"
+import TodoList from "./components/Todolist"
+
+function App() {
+
+  return (
+    <>
+      <div className="main-center">
+        <TodoList />
+      </div>
+    </>
+  )
+}
+
+export default App
